e297a78caf8792b2e3444bd7b2a994ebc5849065
Basuru Yasaruwan <yasaruwanbasuru637@gmail.com>
2025-01-02 14:22:13 +0530

    else if added

--- else_if/src/else_if/Else_if.java
+++ else_if/src/else_if/Else_if.java
@@ -8,13 +8,33 @@ package else_if;
  *
  * @author User
  */
+import java.util.Scanner;
+
 public class Else_if {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number between 1 and 100: ");
+        int num = input.nextInt();
+
+        if (num >= 1 && num <= 20) {
+            System.out.println("The number is in the range of 1 to 20.");
+        } else if (num >= 21 && num <= 40) {
+            System.out.println("The number is in the range of 21 to 40.");
+        } else if (num >= 41 && num <= 60) {
+            System.out.println("The number is in the range of 41 to 60.");
+        } else if (num >= 61 && num <= 80) {
+            System.out.println("The number is in the range of 61 to 80.");
+        } else if (num >= 81 && num <= 100) {
+            System.out.println("The number is in the range of 81 to 100.");
+        } else {
+            System.out.println("The number is out of range.");
+        }
+
     }
-    
+
 }
